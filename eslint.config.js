6b0269eeb0fe374f3bf8dab27db7889dfd648f44
L Angell <lonny.angell@gmail.com>
2025-12-28 14:58:44 -0600

    chore: implement automated linting and testing with husky and lint-staged

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import js from '@eslint/js'
+import typescript from '@typescript-eslint/eslint-plugin'
+import typescriptParser from '@typescript-eslint/parser'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-config-prettier'
+import globals from 'globals'
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: typescriptParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.es2020
+      }
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh
+    },
+    rules: {
+      ...typescript.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true }
+      ],
+      '@typescript-eslint/no-explicit-any': 'off'
+    }
+  },
+  prettier,
+  {
+    ignores: ['dist', 'node_modules', '.github']
+  }
+]
